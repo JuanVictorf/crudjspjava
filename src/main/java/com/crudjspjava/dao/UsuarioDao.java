229e9fee2097a381697339a86b9477075e3a7655
Jose Edson <jose.correa@mv.com.br>
2021-10-13 14:45:03 -0300

    Primeiro commit, tela criada

---
+++ src/main/java/com/crudjspjava/dao/UsuarioDao.java
@@ -0,0 +1,56 @@
+package com.crudjspjava.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jasper.tagplugins.jstl.core.Catch;
+
+import com.crudjspjava.bean.Usuario;
+import com.mysql.jdbc.PreparedStatement;
+
+public class UsuarioDao {
+	public static Connection getConnection() {
+		Connection con = null;
+	
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crudjspjava", "root", "");
+	}catch (Exception e) {
+		System.out.println(e);
+	}
+	return con;
+	}
+	
+	
+	public static List<Usuario> getAllUsuarios(){
+		List<Usuario> list = new ArrayList<Usuario>();
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM USUARIO");
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setId(rs.getInt("id"));
+				usuario.setNome(rs.getString("nome"));
+				usuario.setPassword(rs.getString("password"));
+				usuario.setSexo(rs.getString("sexo"));
+				usuario.setPais(rs.getString("pais"));
+				list.add(usuario);
+			}
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return list;
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/webapp/viewusuarios.jsp
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Crud em Java & HTML</title>
+</head>
+<body>
+	<%@ page import="com.crudjspjava.dao.UsuarioDao, com.crudjspjava.bean.*, java.util.*"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+    
+    <h1>Listagem de usuários</h1>
+    
+    <%
+   	List<Usuario> list = UsuarioDao.getAllUsuarios();
+    request.setAttribute("list", list);
+    %>
+    
+    <table>
+    	<tr><th>ID</th><th>Nome</th><th>Sobrenome</th><th>Password</th><th>Email</th><th>Sexo</th><th>Pais</th></tr>
+    	
+    	<c:forEach items="${list}" var="usuario">
+    		<tr>
+    			<td>${usuario.getId()}</td>
+    			<td>${usuario.getNome()}</td>
+    			<td>${usuario.getPassword()}</td>
+    			<td>${usuario.getEmail()}</td>
+    			<td>${usuario.getSexo()}</td>
+    			<td>${usuario.getPais()}</td>
+    		</tr>
+    	</c:forEach>
+    	
+    </table>
+    
+</body>
+</html>
